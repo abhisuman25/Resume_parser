f9a9fa2e3b9e33e8c564aee276cef4e7376f02a1
milon <milon.mahapatra@stratlytics.com>
2023-05-25 22:58:37 +0530

    I am commiting app.py for deployment purpose

--- app.py
+++ app.py
@@ -1,75 +1,76 @@
-import tkinter as tk
-import pandas as pd
+from pywebio.platform.flask import webio_view
+from pywebio import STATIC_PATH
+from flask import Flask, send_from_directory
+from pywebio.input import *
+from pywebio.output import *
+import argparse
+from pywebio import start_server
+import time
+from resume_scoring import predict_resume_scoring
+app = Flask(__name__)
+import os
+import shutil
 
-def submit_job_description():
-    job_title = job_title_entry.get()
-    qualification = qualification_entry.get()
-    skills = skills_entry.get()
-    work_experience = work_experience_entry.get()
+def cv_parser():
+    
+    
+    put_code("-----<< Welcome to CV Parser Application >>-----")
+    count=0
+    add_more = True
+    
+    while add_more: 
+        
+        info = input_group("Enter Details : ",[
+          input("Enter Job Title you are looking for :", name='job_title'),
+          input("Enter Skillset you are looking for :", name='skill'),
+          input("Enter Education you are looking for :", name='education'),
+          
+          input("Enter years of experience you are looking for :", name='experience',type=FLOAT)
+        ])
+        print(info['job_title'], info['skill'],info['education'],info['experience'])
+        job_title=info['job_title']
+        skill=info['skill']
+        education = info['education']
+        experience = info['experience']
+        
+            
+        put_text('You have searched fors Job Tile : ',job_title)
+        put_text('Selected Skillset :' ,skill)
+        put_text('Required Degree :' ,education)
+        put_text('Required experience :' ,experience)
+        put_text('------------------------------------------------')
+        #put_text(df)
+        with put_loading():
+            put_code("Plz Wait..We are working on magic !! Fetching top CVs for you..")
+            time.sleep(3)  # Some time-consuming operations
+        put_text("Matched Results : ")
+        
+        
+        selected_resume = predict_resume_scoring(job_title,skill,education,experience)
+        put_table([
+                    {"Job_Title":job_title,"SkillSet":skill,"TopCVFound":selected_resume}
+                ], header=["Job_Title", "SkillSet","TopCVFound"]) 
 
-    # Create a dictionary with the job description data
-    job_description = {
-        'Job Title': [job_title],
-        'Qualification': [qualification],
-        'Skills': [skills],
-        'Work Experience': [work_experience]
-    }
+        put_code("**Note: All the matched CVs are stored in 04_Matched_CVs_pdf folder")   
+        
+        add_more = actions(label="Would you like to search more ?", 
+                        buttons=[{'label': 'Yes', 'value': True}, 
+                                 {'label':'No', 'value': False}])
+        
+        count= count+1
+        put_text("--------------------------")
+        put_text(f"You have searched for {count} CVs | Search results shown above. ")
+        
+        put_text("--------------------------")
+        #clear(scope=- 1) 
+      
+    put_code("----- Thank You for using the CV Parser Application -----")
 
-    # Load existing data from the Excel file, if any
-    try:
-        df = pd.read_excel('job_descriptions.xlsx')
-    except FileNotFoundError:
-        df = pd.DataFrame()
 
-    # Append the new job description to the existing data
-    df = pd.concat([df,pd.DataFrame(job_description)], ignore_index=True)
 
-    # Save the updated data to the Excel file
-    df.to_excel('job_descriptions.xlsx', index=False)
+app.add_url_rule('/cv_parser', 'webio_view', webio_view( cv_parser),
+            methods=['GET', 'POST', 'OPTIONS'])
 
-    # Reset the entry fields after submission
-    job_title_entry.delete(0, tk.END)
-    qualification_entry.delete(0, tk.END)
-    skills_entry.delete(0, tk.END)
-    work_experience_entry.delete(0, tk.END)
 
-# Create the main window
-window = tk.Tk()
-window.title("Job Description")
-window.geometry("800x400")
-window.configure(bg="#F9F7F6")
 
-# Load the background image
-background_image = tk.PhotoImage(file="resume_background.png")
-
-# Create a canvas to display the background image
-canvas = tk.Canvas(window, width=800, height=600)
-canvas.pack(fill="both", expand=True)
-canvas.create_image(0, 0, anchor="nw", image=background_image)
-
-# Create labels for each input field
-job_title_label = tk.Label(window, text="Job Title:", bg="#F9F7F6", fg="#333", font=("Arial", 14, "bold"))
-job_title_label.place(x=50, y=50)
-qualification_label = tk.Label(window, text="Qualification:", bg="#F9F7F6", fg="#333", font=("Arial", 14, "bold"))
-qualification_label.place(x=50, y=100)
-skills_label = tk.Label(window, text="Skills:", bg="#F9F7F6", fg="#333", font=("Arial", 14, "bold"))
-skills_label.place(x=50, y=150)
-work_experience_label = tk.Label(window, text="Work Experience:", bg="#F9F7F6", fg="#333", font=("Arial", 14, "bold"))
-work_experience_label.place(x=50, y=200)
-
-# Create entry fields for each input
-job_title_entry = tk.Entry(window, bg="#FFFFFF", fg="#333", font=("Arial", 14), width=40)
-job_title_entry.place(x=250, y=50)
-qualification_entry = tk.Entry(window, bg="#FFFFFF", fg="#333", font=("Arial", 14), width=40)
-qualification_entry.place(x=250, y=100)
-skills_entry = tk.Entry(window, bg="#FFFFFF", fg="#333", font=("Arial", 14), width=40)
-skills_entry.place(x=250, y=150)
-work_experience_entry = tk.Entry(window, bg="#FFFFFF", fg="#333", font=("Arial", 14), width=40)
-work_experience_entry.place(x=250, y=200)
-
-# Create a button to submit the job description
-submit_button = tk.Button(window, text="Submit", command=submit_job_description, bg="#00A388", fg="#FFFFFF", font=("Arial", 14, "bold"))
-submit_button.place(x=350, y=250)
-
-# Run the GUI event loop
-window.mainloop()
+app.run(host='localhost', port=80)
